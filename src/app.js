c662acae5b49e76acc351ea6345299f557deb47b
Melmelada <breenhsolis@gmail.com>
2022-03-27 15:56:43 -0600

    Agregando licencia, iniciando package, jsone y app

---
+++ src/app.js
@@ -0,0 +1,12 @@
+{
+    "name": "bddproject",
+    "version": "1.0.0",
+    "description": "",
+    "main": "app.js",
+    "scripts": {
+      "test": "echo \"Error: no test specified\" && exit 1"   
+    },
+    "keywords": [],
+    "author": "",
+    "license": "EPL-2.0"
+  }
